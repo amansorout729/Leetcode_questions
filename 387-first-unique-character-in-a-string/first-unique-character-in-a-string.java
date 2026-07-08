085c935bf89ed765418b48fc60235a1f847773db
Aman Sorout <amansorout777@gmail.com>
2026-07-08 10:02:23 +0530

    Time: 6 ms (84.63%) | Memory: 47.2 MB (24.30%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,21 +1,36 @@
 class Solution {
     public int firstUniqChar(String s) {
-        
-        
-        for(int i=0;i<s.length();i++){
-            boolean h=true;
-            for(int j=0;j<s.length();j++){
-                if(i!=j && s.charAt(i)==s.charAt(j)){
-                    h=false;
-                    break;
-                }
-            }
-            if(h==true){
-                return i;
+        int[] freq=new int[26];
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            freq[ch-97]++;
 
+        }
+
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(freq[ch-97]==1){
+                return i;
             }
         }
         return -1;
+        
+        
+        // for(int i=0;i<s.length();i++){
+        //     boolean h=true;
+        //     for(int j=0;j<s.length();j++){
+        //         if(i!=j && s.charAt(i)==s.charAt(j)){
+        //             h=false;
+        //             break;
+        //         }
+        //     }
+        //     if(h==true){
+        //         return i;
+
+        //     }
+        // }
+        // return -1;
 
     }
 }
